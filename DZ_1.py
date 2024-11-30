8b4bcb3800aa737b696e675025f75c53d57a1582
qzwiz <naum385385@gmail.com.com>
2024-11-30 21:27:38 +0200

    поменял

--- DZ_1.py
+++ DZ_1.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-    date_random = datetime.strptime(date, '%Y-%m-%d').date()
-    today = datetime.today().date()
-    delta = date_random - today
-    return delta.days
+    try:
+        date_random = datetime.strptime(date, '%Y-%m-%d').date()
+        today = datetime.today().date()
+        delta = date_random - today
+        return delta.days
+    except ValueError:
+        return "Неправильний формат дати. Використовуйте формат 'YYYY-MM-DD'."
 
 
-print(get_days_from_today("2021-10-09"))
+print(get_days_from_today("2024-12-15"))  
+print(get_days_from_today("13.13.2000"))  
